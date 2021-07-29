1933c48f3e813684060449c03be4f7512dcdcdfd
quic <1276109828@qq.com>
2021-07-29 16:18:04 +0800

    增加时间戳函数

---
+++ src/algorithms/libs/timestamp.cc
@@ -0,0 +1,20 @@
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+std::string timestamp()
+{
+    using namespace std::chrono;
+    using clock = system_clock;
+    
+    const auto current_time_point {clock::now()};
+    const auto current_time {clock::to_time_t (current_time_point)};
+    const auto current_localtime {*std::localtime (&current_time)};
+    const auto current_time_since_epoch {current_time_point.time_since_epoch()};
+    const auto current_milliseconds {duration_cast<milliseconds> (current_time_since_epoch).count() % 1000};
+    
+    std::ostringstream stream;
+    stream << std::put_time (&current_localtime, "%T") << "." << std::setw (3) << std::setfill ('0') << current_milliseconds;
+    return stream.str();
+}
---
+++ src/algorithms/libs/timestamp.h
@@ -0,0 +1,8 @@
+#ifndef GNSS_SDR_TIMESTAMP_H
+#define GNSS_SDR_TIMESTAMP_H
+
+#include <string>
+
+std::string timestamp();
+
+#endif
